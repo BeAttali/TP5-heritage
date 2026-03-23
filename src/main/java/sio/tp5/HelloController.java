5404ca3b0301dce357a2c5d5b50c435b575adddf
BeAttali <Betsalel.ATTALI@ortmontreuil.fr>
2026-03-23 17:34:54 +0100

    commit 3.1

--- src/main/java/sio/tp5/HelloController.java
+++ src/main/java/sio/tp5/HelloController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class HelloController {
 
@@ -14,5 +15,10 @@ public class HelloController {
     @FXML
     private TableView tvTP5;
 
+    @Override
+    public void initialize() {
+        tcDescritption.setCellValueFactory(new PropertyValueFactory<>("description"));
+        tcPrix.setCellValueFactory(new PropertyValueFactory<>("prix"));
+    }
 
 }
