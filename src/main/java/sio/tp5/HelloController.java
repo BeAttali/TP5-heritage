ed39c8dd522a638cff49f5f126870fbd13af4e5d
BeAttali <Betsalel.ATTALI@ortmontreuil.fr>
2026-03-23 17:32:38 +0100

    commit 3

--- src/main/java/sio/tp5/HelloController.java
+++ src/main/java/sio/tp5/HelloController.java
@@ -2,13 +2,17 @@ package sio.tp5;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private TableColumn tcDescritption;
+    @FXML
+    private TableColumn tcPrix;
+    @FXML
+    private TableView tvTP5;
+
+
 }
